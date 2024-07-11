49ca2eb0bbd4428b4cfb48e00f795036f184e323
lucywang720 <57937280+lucywang720@users.noreply.github.com>
2024-07-11 21:43:22 +0800

    Delete main directory

--- main/scripts/modify.sh
+++
@@ -1,6 +0,0 @@
-python -m modification.modify \
-    --save_path save_path \
-    --model_name_or_path models--mistralai--Mistral-7B-v0.1/snapshots/26bca36bde8333b5d7f72e9ed20ccda6a618af24 \
-    --alpha $alpha \
-    --save_model \
-    --toxic_path modification/checkpoint/mistral.pt
